c2eab531acefc0e2e5262f5fae08f2bad3d4c46c
Sabrina Putri Salim <>
2018-07-12 13:56:59 +1000

    finished base template and created new templates.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,17 @@
 cssVars();
+
+$('.nav-prompt').click(function () {
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false' ) {
+        $('.nav-inner').attr('aria-expanded', 'true');
+    } else {
+        $('.nav-inner').attr('aria-expanded', 'false');
+    }
+
+    if ($('.nav-prompt').text() == 'Open Navigation') {
+        $('.nav-prompt').text('Close Navigation');
+    } else {
+        $('.nav-prompt').text('Open Navigation');
+    }
+
+});
